f6b6ee4253dae3216606e88750a6d9fa0ba51256
oladimejiadesina-123 <81874299+oladimejiadesina-123@users.noreply.github.com>
2023-12-09 15:56:59 +0100

    update file

---
+++ tests/test_providers_digitalocean_main.py
@@ -0,0 +1,34 @@
+from cloudproxy.providers.digitalocean.main import do_deployment, do_start
+from tests.test_providers_digitalocean_functions import test_list_droplets, test_create_proxy, test_delete_proxy
+
+
+def test_do_deployment(mocker):
+    mocker.patch(
+        'cloudproxy.providers.digitalocean.main.list_droplets',
+        return_value=test_list_droplets(mocker)
+    )
+    mocker.patch(
+        'cloudproxy.providers.digitalocean.main.create_proxy',
+        return_value=test_create_proxy(mocker)
+    )
+    mocker.patch(
+        'cloudproxy.providers.digitalocean.main.delete_proxy',
+        return_value=test_delete_proxy(mocker)
+    )
+    assert do_deployment(1) == 1
+
+
+def test_initiatedo(mocker):
+    mocker.patch(
+        'cloudproxy.providers.digitalocean.main.do_deployment',
+        return_value=2
+    )
+    mocker.patch(
+        'cloudproxy.providers.digitalocean.main.do_check_alive',
+        return_value=["192.1.1.1"]
+    )
+    mocker.patch(
+        'cloudproxy.providers.digitalocean.main.do_check_delete',
+        return_value=True
+    )
+    assert do_start() == ["192.1.1.1"]
