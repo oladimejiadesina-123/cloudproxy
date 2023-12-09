ec8aa49ba855e6ec3d0399ae1c8101a2cf9e1388
oladimejiadesina-123 <81874299+oladimejiadesina-123@users.noreply.github.com>
2023-12-09 15:57:08 +0100

    update file

---
+++ cloudproxy-ui/src/components/Logs.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    {{ data }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListProxies.vue",
+  data() {
+    return {
+      data: {},
+    };
+  },
+  beforeMount() {
+    this.getLogs();
+  },
+  methods: {
+    async getLogs() {
+      const res = await fetch("http://localhost:8000/logs");
+      const data = await res.json();
+      this.data = data;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
